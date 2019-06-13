5a6c0aa735c95c62193ad3b9dc52f6add8115af8
suyash691 <suyash691@live.in>
2019-06-13 17:59:31 +0530

    Create UnitTest1.cs

---
+++ TestProjectWON/UnitTest1.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectWON
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod0()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod01()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod21()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod31()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod02()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod22()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod32()
+        {
+            Assert.AreEqual(2, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod03()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            Assert.AreEqual(2, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod23()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod33()
+        {
+            Debug.Write("Sure to fail test");
+            Console.WriteLine("hello");
+            Assert.AreEqual(2, 4);
+            Debug.Write("Surely failed innit");
+        }
+    }
+}
